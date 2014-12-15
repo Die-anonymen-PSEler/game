5ab67005e4fca093ebc5bb0f713097c345010864
Luca Becker <luca.becker@me.com>
2014-12-15 13:46:53 +0100

    Initial Commit

---
+++ desktop/src/com/retroFactory/retroMachines/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.retroFactory.retroMachines.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.retroFactory.retroMachines.RetroMachines;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RetroMachines(), config);
+	}
+}
