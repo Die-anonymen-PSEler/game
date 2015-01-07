73782ab99118623c72acff46cb9d177e4c2a244c
Luca Becker <luca.becker@me.com>
2015-01-07 14:19:02 +0100

    progress.

--- core/src/com/retroMachines/game/controllers/GameController.java
+++ core/src/com/retroMachines/game/controllers/GameController.java
@@ -1,8 +1,20 @@
 package com.retroMachines.game.controllers;
 
 import com.retroMachines.RetroMachines;
+import com.retroMachines.game.gameelements.RetroMan;
+import com.retroMachines.ui.screens.game.EvaluationScreen;
 import com.retroMachines.ui.screens.game.GameScreen;
+import com.retroMachines.ui.screens.menus.LevelMenuScreen;
+import com.sun.javafx.font.Disposer;
 
+/**
+ * GameController
+ * This class represents the controller for the actual game.
+ * It sets up levels and also disposes them afterwards.
+ * It saves progress to the persistent storage.
+ * @author lucabecker
+ *
+ */
 public class GameController {
 	
 	/**
@@ -11,17 +23,68 @@ public class GameController {
 	private final RetroMachines game;
 	
 	/**
-	 * gameScreen
+	 * The GameScreen for displaying the actual level to the player
 	 */
-	private final GameScreen gameScreen;
+	private GameScreen gameScreen;
+	
+	/**
+	 * The EvaluationScreen when the player triggers this part of the game
+	 */
+	private EvaluationScreen evaluationScreen;
+	
+	private RetroMan retroMan;
 	
 	public GameController(RetroMachines game) {
 		this.game = game;
-		gameScreen = new GameScreen(game, this);
+		retroMan = new RetroMan();
 	}
 	
-	public void loadLevel(int levelId) {
-		
+	/**
+	 * sets initializes a given level and fires it up
+	 * @param levelId the level to be started
+	 */
+	public void startLevel(int levelId) {
+		gameScreen = new GameScreen(game, this);
 		game.setScreen(gameScreen);
+	}
+	
+	private void enterEvaluation() {
+		evaluationScreen = new EvaluationScreen(game, this);
+		game.setScreen(evaluationScreen);
+	}
+	
+	/**
+	 * this method will be called
+	 */
+	public void levelFinished() {
+		saveProgress();
+		dispose();
+		
+		game.setScreen(new LevelMenuScreen(game));
+	}
+	
+	private void dispose() {
+		// TODO Auto-generated method stub
+		gameScreen.dispose();
+		
+	}
+	
+	/**
+	 * saves the progress to the persistent storage
+	 */
+	private void saveProgress() {
+		
+	}
+
+	public void jumpRetroMan() {
+		// TODO Auto-generated method stub
+		if (retroMan.canJump()) {
+			retroMan.jump();
+		}
+	}
+
+	public RetroMan getRetroMan() {
+		// TODO Auto-generated method stub
+		return retroMan;
 	}
 }
--- core/src/com/retroMachines/ui/screens/game/GameScreen.java
+++ core/src/com/retroMachines/ui/screens/game/GameScreen.java
@@ -1,8 +1,11 @@
 package com.retroMachines.ui.screens.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.tests.superkoalio.SuperKoalio.Koala;
 import com.retroMachines.RetroMachines;
 import com.retroMachines.game.controllers.GameController;
 import com.retroMachines.ui.screens.AbstractScreen;
@@ -13,12 +16,40 @@ public class GameScreen extends AbstractScreen {
 	
 	private OrthogonalTiledMapRenderer renderer;
 	
+	private OrthographicCamera camera;
+	
 	private final GameController gameController;
 	
 	
 	public GameScreen(RetroMachines game, GameController gameController) {
 		super(game);
 		this.gameController = gameController;
+		renderer = new OrthogonalTiledMapRenderer(map, 1 / 16f);
+		camera = new OrthographicCamera();
+	}
+	
+	public void setMap(TiledMap map) {
+		this.map = map;
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		
+		inputDetection();
+		
+		camera.position.x = gameController.getRetroMan().getPos().x;
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
+		
+		gameController.getRetroMan().render(delta);
+	}
+	
+	private void inputDetection() {
+		if (Gdx.input.isKeyPressed(Keys.SPACE)) {
+			gameController.jumpRetroMan();
+		}
 	}
 	
 	@Override
