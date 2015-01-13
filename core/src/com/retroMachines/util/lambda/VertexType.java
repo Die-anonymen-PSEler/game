ed3e12cdad7e7090b19f7947e11fdeca970131df
henny256 <uudmi@student.kit.edu>
2015-01-13 12:35:38 +0100

    added JavaDoc to VertexType

--- core/src/com/retroMachines/util/lambda/VertexType.java
+++ core/src/com/retroMachines/util/lambda/VertexType.java
@@ -1,5 +1,28 @@
 package com.retroMachines.util.lambda;
 
+/**
+ * The different types that a vertex could be.
+ * @author RetroFactory
+ *
+ */
 public enum VertexType {
-	NotSetYet ,Abstraction, Application, Variable
+	/**
+	 * The type of the vertex is not set yet.
+	 */
+	NotSetYet ,
+	/**
+	 * The vertex stands for the abstraction of the lambda-term.
+	 * In the game it is represented by a machine.
+	 */
+	Abstraction, 
+	/**
+	 * The vertex stands for the application of the lambda-term.
+	 * In the game it is represented by a light element.
+	 */
+	Application, 
+	/**
+	 * The vertex stands for the variable of the lambda-term.
+	 * In the game it is represented by a metal element.
+	 */
+	Variable
 }
