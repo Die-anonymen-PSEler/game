7ab90a4c5198502ec2d381cc765a15e178d18207
Luca Becker <luca.becker@me.com>
2015-01-14 15:47:04 +0100

    added uml file.

---
+++ core/src/uml.java
@@ -0,0 +1,62 @@
+package com.retroMachines;
+
+/**
+ * @opt views
+ * @hidden
+ * @texignore
+ */
+class UMLOptions {
+        
+}
+
+/**
+ * @view
+ * 
+ * @match class .*
+ * @opt !all
+ * @opt nodefillcolor 1
+ * @opt nodefontname Verdana
+ * @opt edgefontname Verdana
+ * @opt edgecolor #565656
+ * @opt edgefontsize 10
+ * 
+ * @match class com.badlogic.gdx.*
+ * @opt qualify
+ * @opt postfixpackage
+ *
+ * @match class com.retroMachines.*
+ * @opt all
+ * @opt nodefillcolor 2
+ *
+ * @match class com.retroMachines.game.controller.*
+ * @opt nodefillcolor 3
+ * 
+ * @match class com.retroMachines.game.gameelements.*
+ * @opt nodefillcolor 4
+ * 
+ * @match class com.retroMachines.ui.screens.game.*
+ * @opt nodefillcolor 5
+ *
+ * @match class com.retroMachines.ui.screens.menus.*
+ * @opt nodefillcolor 11
+ *
+ * @match class com.retroMachines.util.*
+ * @opt nodefillcolor 12
+ * 
+ * @match class com.retroMachines.data.*
+ * @opt nodefillcolor #ee7600
+ *
+ * @match class com.retroMachines.data.models.*
+ * @opt nodefillcolor #458b74
+ * 
+ * @match class com.retroMachines\..*Exception
+ * @opt nodefillcolor #b22222
+ * @opt nodefontcolor white
+ * 
+ * @match class java.*
+ * @opt hide
+ * 
+ * @texignore
+ */
+public class uml {
+}
