17787bca8b7260aa9c765e02f80757e941322d8b
Luca Becker <luca.becker@me.com>
2015-01-21 13:29:29 +0100

    LambdaUtil class

---
+++ core/src/com/retroMachines/util/lambda/LambdaUtil.java
@@ -0,0 +1,42 @@
+package com.retroMachines.util.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaUtil {
+	
+	private List<OnNextLambdaStepListener> observers;
+	
+	public LambdaUtil() {
+		observers = new ArrayList<OnNextLambdaStepListener>();
+	}
+	
+	public void registerNewListener(OnNextLambdaStepListener listener) {
+		if (!observers.contains(listener)) {
+			observers.add(listener);
+		}
+	}
+	
+	public void unregisterNewListener(OnNextLambdaStepListener listener) {
+		observers.remove(listener);
+	}
+	
+	
+	public void performEvaluation() {
+		while (true) {
+			// TODO weird lambda evaluation stuff
+			for (OnNextLambdaStepListener listener : observers) {
+				listener.nextLambdaStepPerformed();
+			}
+		}
+	}
+	
+	public interface OnNextLambdaStepListener {
+		
+		/**
+		 * this method will be called whenever one step of the lambda evaluation has been completed.
+		 */
+		public void nextLambdaStepPerformed();
+		
+	}
+}
