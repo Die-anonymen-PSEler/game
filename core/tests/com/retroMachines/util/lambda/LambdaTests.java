cc1c9772634e4d96d1002b54395eb8e553c0708e
Luca Becker <luca.becker@me.com>
2015-01-28 11:36:21 +0100

    moved test case to lambda

--- core/tests/com/retroMachines/util/lambda/LambdaTests.java
+++ core/tests/com/retroMachines/util/lambda/LambdaTests.java
@@ -1,11 +1,9 @@
-package com.retroMachines.data.models;
+package com.retroMachines.util.lambda;
 
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.retroMachines.util.lambda.*;
