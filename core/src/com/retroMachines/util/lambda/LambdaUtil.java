524a37b640eca30128108438667128b1648d6b9f
Maik Wiesner <maikwis@gmail.com>
2015-01-30 15:15:26 +0100

    started to implement createTreeFromJason, json file is read in

--- core/src/com/retroMachines/util/lambda/LambdaUtil.java
+++ core/src/com/retroMachines/util/lambda/LambdaUtil.java
@@ -1,11 +1,20 @@
 package com.retroMachines.util.lambda;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 
 public class LambdaUtil {
 	
 	private List<OnNextLambdaStepListener> observers;
+	
+	private Tree tree;
+	private Tree target;
+	private Tree hint;
 	
 	public LambdaUtil() {
 		observers = new ArrayList<OnNextLambdaStepListener>();
@@ -14,8 +23,14 @@ public class LambdaUtil {
 	/**
 	 * creates LambdaTree based on JSON description of level
 	 */
-	public Tree createTreeFromJason() {
-		return null;
+	public Tree createTreeFromJason(String fileName) {
+		tree = new Tree(null);
+		FileHandle file = new FileHandle(new File(fileName));
+		JsonReader reader = new JsonReader();
+		JsonValue value = reader.parse(file);
+		JsonValue JTree = value.get("level").get("data").get("Tree"); //contains tree object
+		System.out.println(JTree.toString());
+		return tree;
 	}
 	
 	public void registerNewListener(OnNextLambdaStepListener listener) {
