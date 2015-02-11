3a6d31d40be0b4f45bcb54b9418bab729c2ab18b
Maik Wiesner <maikwis@gmail.com>
2015-02-11 17:12:47 +0100

    added some lambda stuff to LevelBuilder

--- core/src/com/retroMachines/util/LevelBuilder.java
+++ core/src/com/retroMachines/util/LevelBuilder.java
@@ -2,19 +2,22 @@ package com.retroMachines.util;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.retroMachines.data.AssetManager;
+import com.retroMachines.util.lambda.LambdaUtil;
 import com.retroMachines.util.lambda.data.Tree;
 
 public class LevelBuilder {
 	
 	private TiledMap map;
 	
-	private Tree tree;
+	private LambdaUtil util;
 	
 	/**
 	 * Creates a new instance of the levelbuilder
 	 */
 	public LevelBuilder() {
-		
+		util = new LambdaUtil();
+		//TODO: insert paramater for json path
+		util.createTreeFromJson("some path");
 	}
 	
 	/**
@@ -29,8 +32,8 @@ public class LevelBuilder {
 	 * 
 	 * @return
 	 */
-	public Tree getLambda() {
-		return tree;
+	public LambdaUtil getLambda() {
+		return util;
 	}
 	
 	/**
