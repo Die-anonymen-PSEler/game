7c3ca135abe1e8985aaf50d360223c6d8a784b10
Chibchibi <adrian-schulte@gmx.de>
2015-02-12 21:23:07 +0100

    create method add gameelements to map

--- core/src/com/retroMachines/util/LevelBuilder.java
+++ core/src/com/retroMachines/util/LevelBuilder.java
@@ -1,14 +1,23 @@
 package com.retroMachines.util;
 
+import java.util.LinkedList;
+
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSets;
 import com.retroMachines.data.AssetManager;
+import com.retroMachines.game.gameelements.GameElement;
 import com.retroMachines.util.lambda.LambdaUtil;
+import com.retroMachines.util.lambda.Vertex;
 import com.retroMachines.util.lambda.data.Tree;
 
 public class LevelBuilder {
 	
 	
-	private static final String pathJSON = "maps/LevelJsons/Level";
+	private static final String PATHTOJSON = "maps/LevelJsons/Level";
+	private static final String TILESETNAME_MACHINE = "Maschinen";
+	private static final String TILESETNAME_METALOBJECTS = "Metallobjekte";
+	private static final String TILESETNAME_LIGHT = "Ampeln";
 	private TiledMap map;
 	
 	private LambdaUtil lambdaUtil;
@@ -43,7 +52,18 @@ public class LevelBuilder {
 	public void prepare(int id) {
 		map = AssetManager.loadMap(id);
 		int pathId = id + 1;
-		String pathToLevelJson = pathJSON + pathId;
+		String pathToLevelJson = PATHTOJSON + pathId;
 		lambdaUtil.createTreeFromJson(pathToLevelJson);
+	}
+	
+	private void addGameelements() {
+		TiledMapTileSets levelSets = map.getTileSets();
+		TiledMapTileSet objects = levelSets.getTileSet(TILESETNAME_METALOBJECTS);
+		TiledMapTileSet machines = levelSets.getTileSet(TILESETNAME_MACHINE);
+		TiledMapTileSet lights = levelSets.getTileSet(TILESETNAME_LIGHT);
+		LinkedList<Vertex> levelelements = lambdaUtil.getVertexList();
+		for (int i = 0; i < levelelements.size(); i++) {
+			//TODO Get type of element and color .... draw on map
+		}
 	}
 }
