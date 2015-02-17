5e89dd337a25db53ea399b347058e78f6c58dd15
Luca Becker <luca.becker@me.com>
2015-02-17 15:05:13 +0100

    bunch of javadoc for retroLevel.

--- core/src/com/retroMachines/game/RetroLevel.java
+++ core/src/com/retroMachines/game/RetroLevel.java
@@ -15,6 +15,11 @@ import com.retroMachines.util.Constants;
 import com.retroMachines.util.lambda.LambdaUtil;
 import com.retroMachines.util.lambda.Vertex;
 
+/**
+ * a class containing all relevant information regarding a level 
+ * @author RetroFactory
+ *
+ */
 public class RetroLevel {
 	
 	private int levelId;
@@ -23,20 +28,39 @@ public class RetroLevel {
 	
 	private TiledMap map;
 	
-	public RetroLevel(int id, TiledMap map, LambdaUtil util) {
+	/**
+	 * private constructor so levelbuilder class has to be used
+	 * @param id
+	 * @param map
+	 * @param util
+	 */
+	private RetroLevel(int id, TiledMap map, LambdaUtil util) {
 		this.levelId = id;
 		this.map = map;
 		lambdaUtil = util;
 	}
 
+	/**
+	 * Get method for the map
+	 * @return the map associated with this level
+	 */
 	public TiledMap getMap() {
 		return map;
 	}
 	
+	/**
+	 * returns the lambda util associated with this level
+	 * @return
+	 */
 	public LambdaUtil getLambdaUtil() {
 		return lambdaUtil;
 	}
 	
+	/**
+	 * Checks if a given position is suiteable for a gameelement (whether it makes sense)
+	 * @param pos the vector in question
+	 * @return true if valid; false otherwise.
+	 */
 	public boolean isValidGameElementPosition(Vector2 pos) {
 		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(Constants.OBJECT_LAYER_ID);
 		if (layer.getCell((int)pos.x, (int) pos.y) != null) {
@@ -49,6 +73,13 @@ public class RetroLevel {
 		return true;
 	}
 	
+	/**
+	 * Assigns a GameElement a new position and updates it's coordinates within the lambdautil.
+	 * Also adds the element back to the map for rendering.
+	 * You might wanna call the isValidGameElementPosition first
+	 * @param element the element which will be readded to the map
+	 * @param newPos the position where the element will be added.
+	 */
 	public void placeGameElement(GameElement element, Vector2 newPos) {
 		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(Constants.OBJECT_LAYER_ID);
 		Vertex vertex = lambdaUtil.getVertex(element);
@@ -58,6 +89,10 @@ public class RetroLevel {
 		layer.setCell((int) newPos.x, (int) newPos.y, cell);
 	}
 	
+	/**
+	 * Removes an Object from the map. This does only remove the visual appearance!
+	 * @param elementPosition the vector where element shall be removed
+	 */
 	public void removeGameElement(Vector2 elementPosition) {
 		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(Constants.OBJECT_LAYER_ID);
 		layer.setCell((int) elementPosition.x, (int) elementPosition.y, null);
@@ -76,6 +111,15 @@ public class RetroLevel {
 		return (cell == null) ? null : lambdaUtil.getGameElement((int) posObj.x, (int) posObj.y);
 	}
 	
+	/**
+	 * Returns an array of rectangles based on the tiles that are within a given area.
+	 * @param startX the beginning x coordinate
+	 * @param startY the beginning y coordinate
+	 * @param endX the end x coordinate
+	 * @param endY the end y coordinate
+	 * @param layerId the id where the rectangles shall be gathered.
+	 * @return an array containing those gathered rectangles
+	 */
 	public Array<Rectangle> getTiles(int startX, int startY, int endX, int endY, int layerId) {
 		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(layerId);
 		Array<Rectangle> tiles = new Array<Rectangle>();
@@ -91,15 +135,31 @@ public class RetroLevel {
 		return tiles;
 	}
 	
+	/**
+	 * LevelBuilder class to generate a retrolevel.
+	 * @author RetroFactory
+	 *
+	 */
 	public static class LevelBuilder {
 		
-		
+		/**
+		 * a pattern where the level json files may be found
+		 */
 		public static final String JSON_PATTERN = "maps/LevelJsons/Level%s.json";
 		
+		/**
+		 * the tiledmap that belongs with the level
+		 */
 		private TiledMap map;
 		
+		/**
+		 * the lambda util associated with the level
+		 */
 		private LambdaUtil lambdaUtil;
 		
+		/**
+		 * the level that was created based on the id
+		 */
 		private RetroLevel level;
 		
 		/**
@@ -109,13 +169,17 @@ public class RetroLevel {
 			lambdaUtil = new LambdaUtil();
 		}
 		
+		/**
+		 * Returns the level that was created once the prepare method was called
+		 * @return the level generated
+		 */
 		public RetroLevel getLevel() {
 			return level;
 		}
 		
 		/**
 		 * prepares both map and the lambda structure for the game controller
-		 * @param id
+		 * @param id the id of the level. range [1 - Constants.MAX_LEVEL]
 		 */
 		public void prepare(int id) {
 			map = AssetManager.loadMap(id);
@@ -125,11 +189,13 @@ public class RetroLevel {
 			level = new RetroLevel(levelId, this.map, this.lambdaUtil);
 		}
 		
+		/**
+		 * adds the gameelements to the map.
+		 */
 		private void addGameelements() {
 			LinkedList<Vertex> levelelements = lambdaUtil.getVertexList();
 			TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(Constants.OBJECT_LAYER_ID);
-			for (int i = 0; i < levelelements.size(); i++) {
-				Vertex v = levelelements.get(i);
+			for (Vertex v : levelelements) {
 				int offset = (Integer) v.getGameElement().getTileSet().getProperties().get("firstgid") - 1;
 				int color = v.getColor();
 				v.getGameElement().setTileId(color + offset);
