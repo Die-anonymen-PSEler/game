d48a16b2b88b10337d385bf3a8d49b4c803cfff1
Chibchibi <adrian-schulte@gmx.de>
2015-03-04 20:26:09 +0100

    Update Equals method made it a bit universal

--- core/src/com/retroMachines/util/lambda/Vertex.java
+++ core/src/com/retroMachines/util/lambda/Vertex.java
@@ -565,21 +565,29 @@ public abstract class Vertex {
 		if (v == null) {
 			return false;
 		}
+		
+		//Compare next
 		//veritices equal only if this.next and v.next are both null or both != null.
 		//For this we use xor. True if, and only if, one statement is true, while the other is false.
-		if(this.next != null ^ v.next != null) {
+		if(this.getnext() != null ^ v.getnext() != null) {
 			return false;
-		} else if(this.next != null) { //in case both are not null, need to compare them.
-			if (!this.next.equals(v)) { //if they do not equal we can return false
+		} else if(this.getnext() != null) { //in case both are not null, need to compare them.
+			if (!this.getnext().equals(v.getnext())) { //if they do not equal we can return false
 				return false;
 			}
 		}
-		if (this.getType().equals(v.getType()) && this.getColor() == v.getColor()) {
-			if (this.family != null) {
-				return this.equals(v.family);
-			} else {
-				return v.family == null;
+		
+		//Compare Family
+		if(this.getfamily() != null ^ v.getfamily() != null) {
+			return false;
+		} else if(this.getfamily() != null) { //in case both are not null, need to compare them.
+			if (!this.getfamily().equals(v.getfamily())) { //if they do not equal we can return false
+				return false;
 			}
+		}
+		// If famaly and next is equals compare color and type if equals they are equals
+		if (this.getType().equals(v.getType()) && this.getColor() == v.getColor()) {
+			return true;
 		} else {
 			return false;
 		}
