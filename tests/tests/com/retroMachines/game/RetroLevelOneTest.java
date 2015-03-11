7e3d0f3276b6d90fac2e6d67fe80fb1c0fe4efa1
Luca Becker <luca.becker@me.com>
2015-03-11 15:44:41 +0100

    extended level tests.

--- tests/tests/com/retroMachines/game/RetroLevelOneTest.java
+++ tests/tests/com/retroMachines/game/RetroLevelOneTest.java
@@ -1,6 +1,10 @@
 package com.retroMachines.game;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -11,7 +15,6 @@ import org.junit.runner.RunWith;
 import com.badlogic.gdx.math.Vector2;
 import com.retroMachines.GdxTestRunner;
 import com.retroMachines.data.AssetManager;
-import com.retroMachines.game.RetroLevel;
 import com.retroMachines.game.RetroLevel.LevelBuilder;
 import com.retroMachines.game.gameelements.GameElement;
 
@@ -25,7 +28,7 @@ public class RetroLevelOneTest {
 		new Vector2(1, 20)
 	};
 	
-	public static final Vector2 depotPosition = new Vector2(50, 16);
+	public static final Vector2 depotPosition = new Vector2(50, 6);
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -39,18 +42,20 @@ public class RetroLevelOneTest {
 	@Before
 	public void setUp() throws Exception {
 		LevelBuilder builder = new LevelBuilder();
-		builder.prepare(1);
+		builder.prepare(0);
 		level = builder.getLevel();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		AssetManager.reloadMap(1);
+		AssetManager.reloadMap(0);
 	}
 
 	@Test
 	public void testDepotcheck() {
 		assertFalse("gameelement already in depot", level.allDepotsFilled());
+		assertNotNull("string message sollte erstellt worden sein", level.getErrorMessage());
+		assertFalse("string sollte nicht leer sein", level.getErrorMessage().equals(""));
 	}
 	
 	@Test
@@ -68,8 +73,44 @@ public class RetroLevelOneTest {
 	}
 	
 	@Test
-	public void testRemoveGameElement() {
-		
+	public void testNonExistingGameElement() {
+		Vector2 notExisting = notEligablePositions[0];
+		GameElement element = level.getGameElement(notExisting);
+		assertNull("dort sollte sich kein objekt befinden",	element);
 	}
-
+	
+	@Test
+	public void testRemoveGameElement() {
+		Vector2 variablePosition = level.getLambdaUtil().getVertexList().get(0).getPosition();
+		GameElement element = level.getGameElement(variablePosition);
+		level.removeGameElement(element, variablePosition);
+		element = level.getGameElement(variablePosition);
+		assertNull("element sollte jetzt null sein", element);
+	}
+	
+	@Test
+	public void testRemoveGamelElementAndPlace() {
+		Vector2 variablePosition = level.getLambdaUtil().getVertexList().get(0).getPosition();
+		GameElement element = level.getGameElement(variablePosition);
+		level.removeGameElement(element, variablePosition);
+		level.placeGameElement(element, depotPosition);
+		element = level.getGameElement(depotPosition);
+		assertNotNull("spielobjekt sollte jetzt wieder existieren", element);
+	}
+	
+	@Test
+	public void testDepotsFilled() {
+		Vector2 variablePosition = level.getLambdaUtil().getVertexList().get(0).getPosition();
+		GameElement element = level.getGameElement(variablePosition);
+		level.removeGameElement(element, variablePosition);
+		level.placeGameElement(element, depotPosition);
+		assertTrue("alle spielelemente sollten plaziert sein", level.allDepotsFilled());
+	}
+	
+	@Test
+	public void testDialogChain() {
+		if (level.getLambdaUtil().hasTutorial()) {
+			assertNotNull("eine dialogchain sollte erstellt werden", level.getDialogChain());
+		}
+	}
 }
---
+++ tests/tests/com/retroMachines/game/RetroLevelTwoTest.java
@@ -0,0 +1,58 @@
+package com.retroMachines.game;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Vector2;
+import com.retroMachines.data.AssetManager;
+import com.retroMachines.game.RetroLevel.LevelBuilder;
+import com.retroMachines.game.gameelements.GameElement;
+import com.retroMachines.util.Constants;
+
+public class RetroLevelTwoTest {
+	
+	private RetroLevel level;
+	
+	private static final Vector2[] depots = {
+		new Vector2(51, 6),
+		new Vector2(54, 6),
+		new Vector2(51, 9)
+	};
+	
+	private static final Vector2 machine = new Vector2(16, 14);
+	
+	private static final Vector2 variableMachine = new Vector2(29, 6);
+	
+	private static final Vector2 variable = new Vector2(38, 13);
+
+	@Before
+	public void setUp() throws Exception {
+		LevelBuilder builder = new LevelBuilder();
+		builder.prepare(1);
+		level = builder.getLevel();
+		GameElement element = level.getGameElement(machine);
+		level.removeGameElement(element, machine);
+		level.placeGameElement(element, depots[0]);
+		element = level.getGameElement(variableMachine);
+		level.removeGameElement(element, variableMachine);
+		level.placeGameElement(element, depots[1]);
+		element = level.getGameElement(variable);
+		level.removeGameElement(element, variable);
+		level.placeGameElement(element, depots[2]);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		AssetManager.reloadMap(1);
+	}
+
+	@Test
+	public void testAllDepotsFilled() {
+		assertTrue("alle depots sollten befüllt sein", level.allDepotsFilled());
+	}
+
+}
