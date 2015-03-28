bc74ecc407444a8cdb1c0b161770e85ef662b189
Luca Becker <luca.becker@me.com>
2015-03-28 18:51:21 +0100

    unused imports

--- core/src/com/retroMachines/ui/screens/menus/MainMenuScreen.java
+++ core/src/com/retroMachines/ui/screens/menus/MainMenuScreen.java
@@ -1,6 +1,5 @@
 package com.retroMachines.ui.screens.menus;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
